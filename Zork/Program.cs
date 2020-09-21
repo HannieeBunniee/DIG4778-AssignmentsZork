763c4d91b28a87ea8b43fadf6d14570fa186f82f
HannieeBunniee <54680570+hanniee227@users.noreply.github.com>
2020-09-21 15:46:49 -0400

    Zork 2.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -5,7 +5,7 @@ using System; //use this so u dont have to type system. over n over
 
 namespace Zork
 {
-
+    
     // made the new class Commands.cs and pasted the code
 
 
@@ -14,13 +14,14 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+            string outputString;
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT) //making the game keep looping and wont end unless quit
             {
+                Console.WriteLine(Rooms[CurrentRoomIndex]); //this make the debug write what room they are in
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
-                string outputString;
                 switch (command)
                 {
                     case Commands.QUIT:
@@ -33,7 +34,7 @@ namespace Zork
                     case Commands.SOUTH:
                     case Commands.EAST:
                     case Commands.WEST:
-                        outputString = $"You moved {command}";
+                        outputString = Move(command) ? $"You move {command}" : "The way is shut!"; // ":" is to write the way is shut when there is nothing else to write in the rooms array
                         break;
                     default:
                         outputString = "Unknown Command";
@@ -41,99 +42,43 @@ namespace Zork
                 }
                 Console.WriteLine(outputString);
             }
-            //string inputString = Console.ReadLine(); //ReadLine take no arguements and return player's input as string (literally rewrite the command we input
-
-            //professor, at this point you already confused me with a bunch of codes that could do the same...
-
-            /*
-            inputString = inputString.ToUpper(); // this line make sure the string case-sensitive are the same(so u can use look instead of typing LOOK) 
-            //('ToUpper' transformed all case to CAP and 'ToLower' do oppisite)
-            if (inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing.");
-            }    
-            else if (inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of white house, with a boarded front door.\nA rybber mat saying 'Welcome to Zork!' lies by the door."); // \n is to skip a line
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-            }
-            */
-
-            //Commands command = ToCommand(inputString.Trim().ToUpper()); //calling the ToCommand function v
-            //Console.WriteLine(command);
+            
         }
-        /*
-        private static Commands ToCommand(string commandString) //This method will accept, as parameter, a string to attempt to convert into commands enum value
-        {
-            Commands command;
-            switch (commandString)
-            {
-                case "QUIT":
-                    command = Commands.QUIT;
-                    break;
 
-                case "LOOK":
-                    command = Commands.LOOK;
-                    break;
-
-                case "NORTH":
-                    command = Commands.NORTH;
-                    break;
-
-                case "SOUTH":
-                    command = Commands.SOUTH;
-                    break;
-
-                case "EAST":
-                    command = Commands.EAST;
-                    break;
-
-                case "WEST":
-                    command = Commands.WEST;
-                    break;
-
-                default:
-                    command = Commands.UNKNOWN;
-                    break;
-            };
-            return command;
-            /*
-            if (commandString == "QUIT")
-            {
-                command = Commands.QUIT;
-            }
-            else if (commandString == "LOOK")
-            {
-                command = Commands.LOOK;
-            }
-            else if (commandString == "NORTH")
-            {
-                command = Commands.NORTH;
-            }
-            else if (commandString == "SOUTH")
-            {
-                command = Commands.SOUTH;
-            }
-            else if (commandString == "EAST")
-            {
-                command = Commands.EAST;
-            }
-            else if (commandString == "WEST")
-            {
-                command = Commands.WEST;
-            }
-            else
-            {
-                command = Commands.UNKNOWN;
-            }
-            return command; 
-            // close this
-
-        }*/
         private static Commands ToCommand(string commandString) => (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
         // ^ somewhere in there have the bool statement to ignore case so it can read lowecase without ToUpper line code
+
+
+        private static string[] Rooms = //this is the array of the room from 0-4
+        {
+            "Forest",
+            "West of House",
+            "Behind House",
+            "Clearing",
+            "Canyon View"
+        };
+        private static int CurrentRoomIndex = 1; //this make the player start at west of the house aka array 1
+
+
+        private static bool Move(Commands command)
+        {
+          bool didMove = false;
+          switch (command)
+          {
+              case Commands.EAST when CurrentRoomIndex < Rooms.Length - 1: //allow player to move east(right) as long as the room array isnt exceed the given room length
+                  CurrentRoomIndex++;
+                  didMove = true;
+                  break;
+
+              case Commands.WEST when CurrentRoomIndex > 0: //allow player to move west(left) as long as the room array is higher than 0
+                    CurrentRoomIndex--;
+                  didMove = true;
+                  break;
+          }
+          return didMove;
+        }
+
+        
+     
     }
 }
