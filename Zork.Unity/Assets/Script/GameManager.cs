72315762b07c89fa0e9b2cd5e9150a6b5ce72cf1
Hanniee <hannieetran42@knights.ucf.edu>
2020-12-01 19:07:58 -0500

    Zork in unity
    did the initial set up for this, pushed it to github just incase i screw it up later

---
+++ Zork.Unity/Assets/Script/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Zork;
+
+public class GameManager : MonoBehaviour
+{
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
